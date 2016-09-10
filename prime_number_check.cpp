08803172a8cc26840a99083961181d6a8e7a4ec1
shivamgupta-gupta <shivamgupta.gupta1996@gmail.com>
2016-09-10 11:14:10 +0530

    Update and rename prime_number_check.cbp to prime_number_check.cpp

---
+++ prime_number_check.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n,i,flag=0;
+    cout<<"Enter number - ";
+    cin>>n;
+    if(n<=1){
+        cout<<n<<" is neither prime nor composite number.";
+    }
+    else{
+        for(i=2;i<=sqrt(n);i++){
+            if(n%i==0){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag==1){
+            cout<<n<<" is not a prime number.";
+        }
+        if(flag==0){
+            cout<<n<<" is a prime number.";
+        }
+    }
+    return 0;
+}
