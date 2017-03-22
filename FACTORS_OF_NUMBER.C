7995737abcf2b5f64988a3e021c28e5adbeacef7
Shivam Gupta <shivamgupta.gupta1996@gmail.com>
2017-03-22 22:22:10 +0530

    Update and rename FACTORSO.C to FACTORS_OF_NUMBER.C

--- FACTORS_OF_NUMBER.C
+++ FACTORS_OF_NUMBER.C
@@ -5,6 +5,7 @@ void main()
 	int j = 0,i,n,a[100],k;
 	clrscr();
 	{
+		printf("Enter number - ");
 		scanf("%d",&n);
 		for(i=1;i<=n;i++){
 			if(n%i==0){
@@ -15,9 +16,16 @@ void main()
 			}
 		}
 		k = j;
+		printf("Factor are - ");
 		for(j=0;j<k;j++){
 			printf("%d,",a[j]);
 		}
 	}
 	getch();
 }
+
+
+
+----------  OUTPUT  -----------
+Enter number - 12
+Factors are - 1,2,3,4,6,12,
